fb282122a763dc0a75d901e21cf966b330b845cb
gwaifaiwong <wang8guihui@gmail.com>
2021-03-07 11:35:28 +0800

    add: mybatis-plus

--- demo/src/main/java/com/gwaifaiwong/demo/MpGenerator.java
+++ demo/src/main/java/com/gwaifaiwong/demo/MpGenerator.java
@@ -21,7 +21,7 @@ public class MpGenerator {
 
         //step2:配置全局信息，包括输出路径、包名、作者等信息
         GlobalConfig globalConfig = new GlobalConfig();
-        globalConfig.setOutputDir("D:/ProgramLearning/springboot-wgh/demo"+"/src/main/java");
+        globalConfig.setOutputDir(System.getProperty("user.dir")+"/demo/src/main/java");
         globalConfig.setAuthor("gwaifaiwong");
         globalConfig.setOpen(false);
         autoGenerator.setGlobalConfig(globalConfig);
